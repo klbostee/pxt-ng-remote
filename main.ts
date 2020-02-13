42b66a2e94ebde574f760eda1954be731c6ef902
Klaas Bosteels <klaas.bosteels@gmail.com>
2020-02-13 21:32:53 +0100

    First functional version

--- main.ts
+++ main.ts
@@ -1,7 +1,24 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+
+radio.setGroup(23)
+
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(5)
+})
+
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(11)
+})
+
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(511)
+})
+
+input.onGesture(Gesture.Shake, function () {
+    radio.sendString("shake")
+})
+
+radio.onReceivedNumber(function (receivedNumber: number) {
+    if (receivedNumber > 0) {
+        basic.showNumber(receivedNumber)
+    }
+})
