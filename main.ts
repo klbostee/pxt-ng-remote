fb767c73b856465e6317b8eb1f2eccd2c0e6c21c
Klaas Bosteels <klaas.bosteels@gmail.com>
2020-02-20 23:58:35 +0100

    Second generation remote

--- main.ts
+++ main.ts
@@ -1,12 +1,39 @@
 
 radio.setGroup(23)
 
+let isDocked = false
+let dockedCheck = false
+let manualCounter = 0
+
+function moveServo(score: number) {
+    if (score > 0) {
+        const angle = 190 - score * 18
+        pins.servoWritePin(AnalogPin.P0, angle)
+        basic.showNumber(score)
+    } else {
+        basic.clearScreen()
+        pins.servoWritePin(AnalogPin.P0, 90)
+        basic.pause(2000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+}
+
 input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(5)
+    if (isDocked) {
+        manualCounter += 1
+        moveServo(manualCounter)
+    } else {
+        radio.sendNumber(5)
+    }
 })
 
 input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(11)
+    if (isDocked) {
+        manualCounter = 0
+        moveServo(manualCounter)
+    } else {
+        radio.sendNumber(11)
+    }
 })
 
 input.onButtonPressed(Button.AB, function () {
@@ -17,13 +44,29 @@ input.onGesture(Gesture.Shake, function () {
     radio.sendString("shake")
 })
 
-radio.onReceivedNumber(function (receivedNumber: number) {
-    if (receivedNumber > 0) {
-        const angle = 165 - receivedNumber * 15
-        pins.servoWritePin(AnalogPin.P0, angle)
-        basic.showNumber(receivedNumber)
-    } else {
+basic.forever(function () {
+    dockedCheck = false
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(100)
+    if (input.pinIsPressed(TouchPin.P2)) {
+        dockedCheck = true
+    }
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    if (isDocked != dockedCheck) {
+        isDocked = dockedCheck
+        if (isDocked) {
+            basic.showIcon(IconNames.Yes)
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+        basic.pause(2000)
         basic.clearScreen()
-        pins.servoWritePin(AnalogPin.P0, 175)
+    }
+    basic.pause(2000)
+})
+
+radio.onReceivedNumber(function (receivedNumber: number) {
+    if (isDocked) {
+        moveServo(receivedNumber)
     }
 })
